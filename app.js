c5ddffd2c45194becf55b34e674170b8500b8595
Venus DM <crhowe90@gmail.com>
2022-07-17 21:44:49 -0500

    fs module

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    //this...
-    for (let i=0; i < profileDataArr.length; i += 1){
-    console.log(profileDataArr[i]);
-    }
-    console.log("=============");
-    
-    //is the same as this...
-    profileDataArr.forEach((profileItem)=> console.log(profileItem));
-};
+const generatePage = require('./src/page-template.js');
 
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile("./index.html", generatePage(name, github), err =>{
+    if(err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
